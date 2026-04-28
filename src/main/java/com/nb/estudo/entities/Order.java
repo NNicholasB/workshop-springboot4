fbab3a52582d711d37a726446d2b14c29af04011
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-04-27 22:13:00 -0300

    v2

--- src/main/java/com/nb/estudo/entities/Order.java
+++ src/main/java/com/nb/estudo/entities/Order.java
@@ -1,5 +1,7 @@
 package com.nb.estudo.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -13,7 +15,9 @@ public class Order implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @JsonFormat(shape=JsonFormat.Shape.STRING,pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",timezone = "GMT")
     private Instant  moment;
+
     @ManyToOne
     @JoinColumn(name="client_id")
     private User client;
