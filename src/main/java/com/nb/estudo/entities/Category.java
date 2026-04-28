2c53665a5491ad45624be690ee086dab6a9e770a
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-04-28 20:52:03 -0300

    MTM with jointable

--- src/main/java/com/nb/estudo/entities/Category.java
+++ src/main/java/com/nb/estudo/entities/Category.java
@@ -17,6 +17,8 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @ManyToMany(mappedBy="categories")
     private Set<Product> products=new HashSet<>();
     public Category(){}
     public Category(Long id, String name) {
